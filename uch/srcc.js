fa69035b45a3879998101d68fbf3e0bd09cdc4f0
fastarms <57482753+fastarms@users.noreply.github.com>
2020-04-29 09:33:02 +0400

    Update srcc.js

--- uch/srcc.js
+++ uch/srcc.js
@@ -1,3 +1,3 @@
 logins=['admin', 'jack', 'fa']
 passwords=['hesoyam', 'loh', 'q']
-links=['../uch/admin/main.html', '../uch/jack/main.html', '../uch/fa/main.html']
+links=['../admin/main.html', '../jack/main.html', '../fa/main.html']
